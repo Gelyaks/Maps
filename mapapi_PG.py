f70dee50783b7ad6f319bef0411755d1c2dd4e2e
Angelina <angelveresh@yandex.ru>
2022-01-31 21:03:23 +0300

    pgup, pgdown

--- mapapi_PG.py
+++ mapapi_PG.py
@@ -6,10 +6,9 @@ import pygame
 c = input('Введите координаты: ').split(', ')
 
 
-def show_map(ll_spn=None, map_type="map", add_params=None):
+def show_map(ll_spn=None, map_type="map", add_params=None, z='0.004,0.0019'):
     if ll_spn:
-        map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&spn=0.004,0.0019&l={map_type}"
-        print(map_request)
+        map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&spn={z}&l={map_type}"
     else:
         map_request = f"http://static-maps.yandex.ru/1.x/?l={map_type}"
     if add_params:
@@ -26,17 +25,27 @@ def show_map(ll_spn=None, map_type="map", add_params=None):
 
     with open(map_file, 'wb') as file:
         file.write(response.content)
+    return map_file
 
-    pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    pygame.display.set_caption('Map')
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+pygame.display.set_caption('Map')
+zoom = [0.004, 0.0019]
+while pygame.event.wait().type != pygame.QUIT:
+    event = pygame.event.wait()
+    if event.type == pygame.QUIT:
+        break
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_PAGEUP and zoom[0] < 7:
+            zoom[0] += 0.4
+        elif event.key == pygame.K_PAGEDOWN and zoom[0] > 0.4:
+            zoom[0] -= 0.4
+    map_file = show_map(ll_spn=f'll={c[1]},{c[0]}', z=f'{zoom[0]},{zoom[1]}')
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
-
-    os.remove(map_file)
+pygame.quit()
+os.remove(map_file)
+# 55.859564, 37.443721
 
 
-show_map(ll_spn=f'll={c[0]},{c[1]}')
